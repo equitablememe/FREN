ac2a932c3bee395f7bdf603bf1b1a8b1eb394411
equitablememe <equitablememe@gmail.com>
2026-08-19 10:37:22 -0700

    Export transport-only adapter boundary

--- src/fren/adapters/__init__.py
+++ src/fren/adapters/__init__.py
@@ -1,10 +1,13 @@
-from .base import AdapterRequest, AdapterResult, FrenAdapter
+from .base import AdapterRequest, FrenAdapter, ProviderResponse
 from .conformance import AdapterAssessment, judge_adapter
+from .normalize import AdapterNormalizationError, normalize_provider_response
 
 __all__ = [
     "AdapterAssessment",
+    "AdapterNormalizationError",
     "AdapterRequest",
-    "AdapterResult",
     "FrenAdapter",
+    "ProviderResponse",
     "judge_adapter",
+    "normalize_provider_response",
 ]
