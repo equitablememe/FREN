705b9eaac198e2e32eb1b419511e43e46e3ad82b
equitablememe <equitablememe@gmail.com>
2026-08-19 10:40:56 -0700

    Export experimental vendor transports

--- src/fren/adapters/__init__.py
+++ src/fren/adapters/__init__.py
@@ -1,13 +1,23 @@
+from .anthropic import AnthropicAdapter
 from .base import AdapterRequest, FrenAdapter, ProviderResponse
 from .conformance import AdapterAssessment, judge_adapter
+from .gemini import GeminiAdapter
+from .http import VendorAdapterError
 from .normalize import AdapterNormalizationError, normalize_provider_response
+from .openai import OpenAIAdapter
+from .xai import XAIAdapter
 
 __all__ = [
     "AdapterAssessment",
     "AdapterNormalizationError",
     "AdapterRequest",
+    "AnthropicAdapter",
     "FrenAdapter",
+    "GeminiAdapter",
+    "OpenAIAdapter",
     "ProviderResponse",
+    "VendorAdapterError",
+    "XAIAdapter",
     "judge_adapter",
     "normalize_provider_response",
 ]
