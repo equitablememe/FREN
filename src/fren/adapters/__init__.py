8a45c90ca3d559da295fa12a0bf31ba0d1e2d890
equitablememe <equitablememe@gmail.com>
2026-08-19 10:14:30 -0700

    Add generic adapter contract

---
+++ src/fren/adapters/__init__.py
@@ -0,0 +1,10 @@
+from .base import AdapterRequest, AdapterResult, FrenAdapter
+from .conformance import AdapterAssessment, judge_adapter
+
+__all__ = [
+    "AdapterAssessment",
+    "AdapterRequest",
+    "AdapterResult",
+    "FrenAdapter",
+    "judge_adapter",
+]
